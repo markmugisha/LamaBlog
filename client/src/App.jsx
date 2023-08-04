014bb03580e93506b74755077f002a9b8fb82c37
Mark Mugisha <mhmugisha@gmail.com>
2023-08-04 11:03:17 +0300

    Pages and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,41 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Write from "./pages/Write";
+import Single from "./pages/Single";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/home",
+    element: <div>
+      <Navbar/>
+      <Home/>
+      <Footer/>
+      </div>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  
+]);
+
 function App() {
-  return <div>Hello World!</div>;
+  return <div>
+    <RouterProvider router={router}/>
+  </div>;
 }
 
 export default App;
